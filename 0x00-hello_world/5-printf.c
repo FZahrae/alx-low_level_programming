a0708024fc774f374af086c27427a71b47c8318c
FZahrae <fkourisse@gmail.com>
2023-02-16 13:43:37 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- *  main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * main - Enty point
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
